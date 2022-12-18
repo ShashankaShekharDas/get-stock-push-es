87c235ff5eab548b437cf5621e780c7f3e3fa06b
Shashanka <gudurajasekarreddy@gmail.com>
2022-12-18 22:54:36 +0530

    Added Scheduling to base main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-# Just for Testing Remove Later
+import time
+
+from get_stock_data_push_to_ES import GET_DATA_PERIOD_MINUTES
 from get_stock_data_push_to_ES.elastic_search import Elastic_Search
 from get_stock_data_push_to_ES.get_data_api import Get_stock_data
+import schedule
 
-Get_stock_data().get_stock_data()
-Elastic_Search().send_data_to_es()
+
+def get_data_and_send_to_es():
+    Get_stock_data().get_stock_data()
+    Elastic_Search().send_data_to_es()
+
+
+schedule.every(GET_DATA_PERIOD_MINUTES).minutes.do(get_data_and_send_to_es)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
